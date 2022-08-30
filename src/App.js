9544d3f99e808edf670c2ab8477a0fb73cc51294
Chukwuka Chidera Princewill <cchukwuka@seamhealthgroup.com>
2022-08-30 02:28:41 +0100

    add info and features to gris

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import { BryntumGrid } from '@bryntum/grid-react';
 import './App.scss';
-import { dataStore } from './store/data-store';
+import { dataStore } from './store/DataStore';
 
 function App() { 
+    const [toggleForm, setToggleForm] = useState(false);
     const [data, setData] = useState(dataStore.data);
     const [columns, setColumns] = useState([
         { text : 'Country Name', field : 'countryName', flex : 2 },
@@ -16,7 +17,20 @@ function App() {
     const [features, setFeatures] = useState({
         cellEdit     : true,
         regionResize : false,
-        stripe: true
+        stripe: true,
+        filter: true,
+        rowReorder: true,
+        columnReorder: true,
+        
+    });
+    const [formData, setFormData] = useState({
+        id: '',
+        countryName: '',
+        code: '',
+        capital: '',
+        continent: '',
+        currency: '',
+        languages: ''
     });
     
     useEffect(() => {
@@ -26,12 +40,121 @@ function App() {
         return () => subscription.unsubscribe();   
     } , []);
 
-    
+    const triggerToggle = () => {
+        setToggleForm(!toggleForm);
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // to increment the id of the new record
+        formData.id = data.length + 1;
+
+        // to add the new record to the data array
+        dataStore.sendData([...data, formData]);
+
+        // to reset the form
+        setFormData({
+            id: '',
+            countryName: '',
+            code: '',
+            capital: '',
+            continent: '',
+            currency: '',
+            languages: ''
+        });
+        // to close the form
+        setToggleForm(!toggleForm);
+
+    }
 
     return (
-        
-        <BryntumGrid data={data}  columns={columns}   features={features} />
-        
+        <>
+            <div className='content-container'>
+                <div className='new-content'>
+                    <button className='btn-primary' onClick={triggerToggle}>Add new</button> 
+                </div>
+
+                { toggleForm && <div>
+                    <form className='form-container'>
+                        <input 
+                            type="text" 
+                            placeholder="Country Name" 
+                            name='countryName'  
+                            id='countryName'
+                            className='form-input' 
+                            onChange={(e) => setFormData({...formData, countryName: e.target.value})} 
+                            value={formData.countryName}
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Code" 
+                            name='code' 
+                            id='code' 
+                            className='form-input'
+                            onChange={(e) => 
+                                setFormData({...formData, 
+                                code: e.target.value})
+                            } 
+                            value={formData.code}
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Capital" 
+                            name='capital'
+                            id='capital'
+                            className='form-input'
+                            onChange={(e) =>
+                                setFormData({...formData,
+                                capital: e.target.value})
+                            }
+                            value={formData.capital}
+                        /> <br />
+                        <input 
+                            type="text" 
+                            placeholder="Continent" 
+                            name='continent'
+                            id='continent'
+                            className='form-input'
+                            onChange={(e) =>
+                                setFormData({...formData,
+                                continent: e.target.value})
+                            }
+                            value={formData.continent}
+                            
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Currency" 
+                            name='currency'
+                            id='currency'
+                            className='form-input'
+                            onChange={(e) =>
+                                setFormData({...formData,
+                                currency: e.target.value})
+                            }
+                            value={formData.currency}
+                        />
+                        <input 
+                            type="text" 
+                            placeholder="Languages" 
+                            name='languages'
+                            id='languages'
+                            className='form-input'
+                            onChange={(e) =>
+                                setFormData({...formData,
+                                languages: e.target.value})
+                            }
+                            value={formData.languages}
+                        />
+                        <div>
+                            <button className='btn-primary' onClick={handleSubmit}>Submit</button>
+                        </div>
+                        <br />
+                    </form>
+                        </div>}
+                <BryntumGrid data={data}  columns={columns}   features={features} />
+            </div>
+        </>
     );
 }
 
